a370528e7e441bc19378db891eb9e17a5b7f4f1a
Jyotsnachappali <jyotsnachappali65@gmail.com>
2025-12-27 10:11:23 +0530

    Update main19.py

--- main19.py
+++ main19.py
@@ -2,7 +2,7 @@ x=list(map(int,input().split()))
 min1=max1=x[0]
 for i in range(1,len(x)):
     if x[i]<min1:
-        min1=x[1]
+        min1=x[i]
     if x[i]>max1:
         max1=x[i]
 print(min1,max1)
